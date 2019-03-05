5d924c311a5f103bf8f3ed14c61e80e2d41ea5da
John Reinier Bulaong <rein.bulaong@paymaya.com>
2019-03-05 16:19:38 +0800

    make application exportable

--- index.js
+++ index.js
@@ -9,16 +9,21 @@ const http = require('http');
 const log = debug("application:api");
 const app = express();
 
-const PORT = process.env.PORT || 0;
-
 app.use("/api", api);
 app.use("/", express.static(path.join(__dirname, "dist")));
 app.use("/", history());
 app.use("/", express.static(path.join(__dirname, "dist")));
 
 const server = http.createServer(app);
-server.listen(PORT, () => {
-  const port = server.address().port
-  log(`listening on port ${port}`);
-  open(`http://localhost:${port}`);
-});
+
+module.exports = server;
+
+if (process.mainModule = module) {
+  const PORT = process.env.PORT || 0;
+  server.listen(PORT, () => {
+    const port = server.address().port
+    log(`listening on port ${port}`);
+    open(`http://localhost:${port}`);
+  });
+}
+
